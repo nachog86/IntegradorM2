4959a7e3a64aa6a75a8a5df5da10cbf48d3dd913
nachog86 <gomezignacio86@gmail.com>
2023-06-24 20:16:38 -0300

    Integracion Completa, falta los diseños de CSS

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -1,14 +1,30 @@
-const users = require('../utils/users');
+const { User } = require('../DB_connection')
 
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.query
 
-module.exports = (req, res) => {
-    const { email, password } = req.query;
-    let acces = false;
-    users.forEach((user) => {
-        if (user.email === email && user.password === password) {
-            acces = true;
+        if (!email || !password) {
+            return res.status(400).send('Faltan datos')
         }
-    })
 
-    return res.status(200).json({acces});
+        const user = await User.findOne({
+            where: { email: email }
+        })
+
+        if (!user) return res.status(404).send('Usuario no encontrado.')
+
+        if (user.password === password) {
+            return res.json({
+                access: true
+            })
+        } else {
+            return res.status(403).send('Contraseña incorrecta.')
+        }
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
 }
+
+module.exports = login;
--- server/src/utils/users.js
+++
@@ -1,3 +0,0 @@
-module.exports = [{
-    email: 'gomezignacio86@gmail.com', password: 'usuario12'
-}]
